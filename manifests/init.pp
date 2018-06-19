1aadd19ae3a97af73f7bdaf06af16cd61f6ad845
Stefan <stefan@puppet.com>
2018-06-19 20:18:36 +0100

    Correct simplified OS handling

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,9 +7,5 @@
 #   include support_sysstat
 class support_sysstat {
   if $facts['os']['family'] =~ /^(RedHat|Debian)$/ { include support_sysstat::install }
-  elsif $facts['kernel'] == 'Linux' {
-    notify{'This module may have unintended consequences on non-EL or Debian/Ubuntu distros.':}
-    include support_sysstat::install
-  }
-  else { warning('This sysstat module does not work on non-Linux nodes.') }
+  else { warning('This sysstat module only supports EL and Debian derivative Linux nodes.') }
 }
